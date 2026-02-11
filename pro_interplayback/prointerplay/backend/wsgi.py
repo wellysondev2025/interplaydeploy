a18205ab2e6bc5bf3919da01d6901d5da55b50bb
Wellyson Machado <wellyson.machado01@gmail.com>
2026-02-10 22:35:38 -0300

    alternado asgi e wsgi

--- pro_interplayback/prointerplay/backend/wsgi.py
+++ pro_interplayback/prointerplay/backend/wsgi.py
@@ -3,7 +3,7 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault(
     'DJANGO_SETTINGS_MODULE',
-    'prointerplay.backend.settings'
+    'backend.settings'
 )
 
 application = get_wsgi_application()
