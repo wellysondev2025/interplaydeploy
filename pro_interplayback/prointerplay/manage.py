362e06468d7bb884bbd21c4517ca0642ea57307b
Wellyson Machado <wellyson.machado01@gmail.com>
2026-02-10 22:28:58 -0300

    mexendo no manage tirando sys

--- pro_interplayback/prointerplay/manage.py
+++ pro_interplayback/prointerplay/manage.py
@@ -2,18 +2,12 @@
 import os
 import sys
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-sys.path.append(os.path.join(BASE_DIR, "prointerplay"))
-
 def main():
     os.environ.setdefault(
         'DJANGO_SETTINGS_MODULE',
         'backend.settings'
     )
-    try:
-        from django.core.management import execute_from_command_line
-    except ImportError as exc:
-        raise
+    from django.core.management import execute_from_command_line
     execute_from_command_line(sys.argv)
 
 if __name__ == '__main__':
